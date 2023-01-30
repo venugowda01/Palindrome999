3cc8f569ccef50c58e8d70dd89e1f80e964fabf1
Venu Gopal <Venu Gopal@LAPTOP-MUVMLS8I>
2023-01-30 00:27:28 -0800

    Add project files.

---
+++ Palindrome99/Recursion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class F_Palindrome_R
+{
+    public static bool IsPalindrome(string text)
+    {
+        if (text.Length <= 1)
+            return true;
+        else
+        {
+            if (text[0] != text[text.Length - 1])
+                return false;
+            else
+                return IsPalindrome(text.Substring(1, text.Length - 2));
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(IsPalindrome("radar"));
+        Console.WriteLine(IsPalindrome("pato"));
+    }
+}
